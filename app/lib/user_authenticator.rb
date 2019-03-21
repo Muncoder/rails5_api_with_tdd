243e961bc7e320250d865b82de4a72abed429957
Munir Uddin <munirchittagong@yahoo.com>
2019-03-21 19:49:44 +0000

    38. Login user endpoints unauthorized

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -8,17 +8,14 @@ class UserAuthenticator
   end
 
   def perform
+    raise AuthenticationError if code.blank?
+    raise AuthenticationError if token.try(:error).present?
 
-    if token.try(:error).present?
-      raise AuthenticationError
-    else
-      prepare_user
-      @access_token = if user.access_token.present?
-                user.access_token
-               else
-                 user.create_access_token
-               end
-
+    prepare_user
+    @access_token = if user.access_token.present?
+        user.access_token
+      else
+        user.create_access_token
     end
   end
 
