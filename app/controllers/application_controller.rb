9fb17dd65c920e0962f8ae97d11524e04c361807
Munir Uddin <munirchittagong@yahoo.com>
2019-03-24 11:54:00 +0000

    41. Authorization error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
+  class AuthorizationError < StandardError; end
   rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :authorization_error
 
   private
 
@@ -10,7 +12,16 @@ class ApplicationController < ActionController::API
         "title" =>  "Authentication code is invalid",
         "detail" => "You must provide valid code in order to exchange it for token."
     }
-
     render json: { "errors": [ error ] }, status: 401
+  end
+
+  def authorization_error
+    error = {
+      "status" => "403",
+      "source" => { "pointer" => "/headers/authorization" },
+      "title" =>  "Not authorized",
+      "detail" => "You have no right to access this resource."
+    }
+    render json: { "errors": [ error ] }, status: 403
   end
 end
